d6ffd28d6191857905ab5bfd33929b20eb78002d
Vasil Evgeniev Aleksandrov <vvvaleksandrovv@gmail.com>
2022-09-16 11:50:26 +0300

    added bootstrap

---
+++ herokupoker/src/app/user-card/user-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.css']
+})
+export class UserCardComponent implements OnInit {
+  username = "Juli";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
