487235ba639cfc62e46fc2973b703669d2cd3d43
Vasil Evgeniev Aleksandrov <vvvaleksandrovv@gmail.com>
2022-09-16 13:18:36 +0300

    added cards

--- herokupoker/server.js
+++ herokupoker/server.js
@@ -1,8 +1,17 @@
 //Install express server
 const express = require('express');
+// const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/pokerstats',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+// );
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/herokupoker'));
--- herokupoker/src/app/app.component.ts
+++ herokupoker/src/app/app.component.ts
@@ -8,6 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   btntitle = 'herokupoker';
 
+  users = [
+    {username: 'Юли Маймуната', earnings: 0, costs: 0, imgUrl: 'assets/images/julilud.png'},
+    {username: 'Васо Коня', earnings: 0, costs: 0, imgUrl: 'assets/images/vasil.png'},
+    {username: 'Лъчи BadBeat Балъчи', earnings: 0, costs: 0, imgUrl: 'assets/images/lucho.png'},
+    {username: 'Андрей Умрелия', earnings: 0, costs: 0, imgUrl: 'assets/images/andrei.png'},
+    {username: 'Замера Дъртата Мърша', earnings: 0, costs: 0, imgUrl: 'assets/images/zamera.png'},
+    {username: 'Вацун Лъвъ', earnings: 0, costs: 0, imgUrl: 'assets/images/vacata.png'},
+    {username: 'Илийчо Камилийчо', earnings: 0, costs: 0, imgUrl: 'assets/images/kamil.png'},
+    {username: 'Валю Дългия', earnings: 0, costs: 0, imgUrl: 'assets/images/valio.png'},
+    {username: 'Светлю Паса', earnings: 0, costs: 0, imgUrl: 'assets/images/sve.png'},
+  ];
+
   hello() {
     this.btntitle = "JULI E MRUSNA KURVAA"
   }
